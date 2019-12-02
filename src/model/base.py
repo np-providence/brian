7edc5432fd4db0dc593f62dd3db696ab62d2b9c6
Sean Lim <seanlimdev856@gmail.com>
2019-12-02 15:44:55 +0800

    Fix some shit

--- src/model/base.py
+++ src/model/base.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#engine = create_engine('postgresql://postgres:mysecretpassword@localhost:5432/postgres')
 engine = create_engine('postgresql://postgres:mysecretpassword@postgresdb:5432/brian')
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
