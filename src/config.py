23baec2db64b9e80a8696198da3cee90c13bfb61
shunyuan <tanshunyuan99@gmail.com>
2020-01-15 23:00:31 +0800

    Updated backend endpoint

--- src/config.py
+++ src/config.py
@@ -11,8 +11,8 @@ class ConfigClass(object):
     SECRET_KEY = 'This is an INSECURE secret!! DO NOT use this in production!!'
 
     # Flask-SQLAlchemy settings
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:mysecretpassword@postgresdb/brian
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:mysecretpassword@localhost:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:mysecretpassword@postgresdb/brian'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:mysecretpassword@localhost:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Avoids SQLAlchemy warning
 
     JWT_SECRET_KEY = os.getenv('SECRET')
