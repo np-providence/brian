d216b20fed1d927cacfeb4bd86f31fc482a68e25
Shun Yuan <tanshunyuan99@gmail.com>
2020-01-30 22:15:35 +0800

    Added email sending function and updated .env.example

--- src/common/common.py
+++ src/common/common.py
@@ -1,9 +1,37 @@
+import smtplib
+import os
 from datetime import datetime, timedelta
 from contextlib import contextmanager
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 db = SQLAlchemy()
+
+
 def gen_hash():
     today = datetime.now()
     return abs(hash(today))
 
+
+def send_email(subject, msg):
+    email = os.getenv('EMAIL')
+    password = os.getenv('PASSWORD')
+    try:
+        server = smtplib.SMTP('smtp.office365.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(email, password)
+        message = 'Subject: {}\n\n{}'.format(subject, msg)
+        server.sendmail(email, email, message)
+        server.quit()
+        print('Email sent')
+    except Exception as e:
+        print(e)
+
+
+# Sample
+#subject = "MEMEM"
+#message = "This is a meme"
+#send_email(subject, message)
